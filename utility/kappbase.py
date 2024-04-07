573bbaabffc36ebe0501165a0e138557cefda9dc
Liu, Kezhao <kezhao.liu@intel.com>
2024-04-07 22:28:00 +0800

    Release version: v0.2.1

--- utility/kappbase.py
+++ utility/kappbase.py
@@ -1,9 +1,11 @@
 from PyQt5.QtGui import QPixmap
 
 from about_ui_qt5 import *
+import config_ui_qt5 as cfg_ui_dialog
 from utility.software_mgr import *
 from utility.kworkthread import *
 from utility.kmisc import *
+from PyQt5.QtCore import QEvent
 
 import pandas as pd
 import numpy as np
@@ -15,7 +17,7 @@ class KAppBase(object):
         self.mainwindow = MainWindow
         self.splash_check()
 
-        self.mainwindow.setWindowTitle('AutoAllegro v0.1')
+        self.mainwindow.setWindowTitle('AutoAllegro v0.2')
 
         self.kwork_thread = KWorkThread()
         self.kwork_thread.start()
@@ -25,11 +27,12 @@ class KAppBase(object):
         self.ui_mgr.splash_check()
         self.software_mgr = Software_Mgr('autoallegro', 'AutoAllegro', MainWindow)
         self.software_mgr.check_software_version_background(self.ui_mgr)
-        self.button_mgr = Button_Mgr(ui, MainWindow, self.kwork_thread)
-        self.icon_mgr = Icon_Mgr(ui, MainWindow)
-        self.about_window_mgr = About_Window_Mgr(ui)
-
         self.table_mgr = Table_Mgr(ui, MainWindow, self.kwork_thread)
+        self.config_dialog_mgr = Config_Dialog_Mgr(ui, self.kwork_thread, self.table_mgr)
+        self.button_mgr = Button_Mgr(ui, MainWindow, self.kwork_thread, self.config_dialog_mgr, self.table_mgr)
+        self.icon_mgr = Icon_Mgr(ui, MainWindow)
+        self.about_dialog_mgr = about_dialog_mgr(ui)
+        self.config_dialog_mgr.signal_child_2_parent.connect(self.child_message_handler)
 
         pass
 
@@ -42,6 +45,11 @@ class KAppBase(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
+    def child_message_handler(self, param_1):
+        print('[main thread] main windows received child signal ' + param_1)
+        xlsx_path = param_1
+        print('xlsx_path: ' + xlsx_path)
+        self.kwork_thread.send_work_message([3, xlsx_path])
     def work_thread_signal_handler(self, param_1, param_2, param_3):
 
         print('[main thread] ui slots received signal id: ' + param_1)
@@ -66,6 +74,12 @@ class KAppBase(object):
             self.ui.pushButton_2.setEnabled(True)
             self.ui.pushButton_2.setText('Write')
             self.ui.pushButton_2.setStyleSheet("color: black")
+
+            if str(self.data2) == '0':
+                QMessageBox.information(self.mainwindow, 'Warning', 'Invalid dcfx format, please select correct dcfx')
+                self.config_dialog_mgr.exec()
+                return
+
         if self.id == '3':
             self.table_mgr.df = self.data2
             self.table_mgr.init_table_default()
@@ -108,12 +122,14 @@ class UI_Mgr:
         # splash support close pic end
 
 class Button_Mgr:
-    def __init__(self, ui, main_window, kwork_thread):
+    def __init__(self, ui, main_window, kwork_thread, config_dialog_mgr, table_mgr):
         self.ui = ui
         self.main_window = main_window
         self.kwork_thread = kwork_thread
+        self.config_dialog_mgr = config_dialog_mgr
+        self.table_mgr = table_mgr
         self.set_signal_connect()
-        self.ui.pushButton.setEnabled(False)
+        #self.ui.pushButton.setEnabled(False)
 
     def set_signal_connect(self):
         self.ui.pushButton.clicked.connect(lambda: self.on_button_clicked_read(255, 255, 255))
@@ -122,38 +138,172 @@ class Button_Mgr:
 
     def on_button_clicked_read(self, a, b, c):
         function_enter()
-        self.kwork_thread.send_work_message([1, 'data_1'])
+        #self.kwork_thread.send_work_message([1, 'data_1'])
         function_exit()
+        self.config_dialog_mgr.exec()
         pass
     def on_button_clicked_modifier(self, a, b, c):
 
-        rvp_cus = {}
-        for row in range(self.ui.tableWidget.rowCount()):
-            for col in range(self.ui.tableWidget.columnCount()):
-                if row == 0:
-                    continue
-                value = self.ui.tableWidget.item(row, col).text()
-                if col == 1 or col == 3:
-                    rvp_cus[self.ui.tableWidget.item(row, col).text()] = self.ui.tableWidget.item(row, col + 1).text()
-                print('row:' + str(row) + ',  col:' + str(col) + '\n' + value)
+        if not self.config_dialog_mgr.dcfx_path or not self.config_dialog_mgr.xlsx_path:
+            print('dcfx or xlsx is null')
+            if not self.config_dialog_mgr.dcfx_path and not self.config_dialog_mgr.xlsx_path:
+                QMessageBox.information(self.main_window, 'Warning', 'Please configure xlsx/dcfx path.')
+            elif not self.config_dialog_mgr.dcfx_path:
+                QMessageBox.information(self.main_window, 'Warning','Please configure dcfx path.')
+            elif not self.config_dialog_mgr.xlsx_path:
+                QMessageBox.information(self.main_window, 'Warning','Please configure xlsx path.')
+            self.config_dialog_mgr.exec()
+            pass
+        else:
+            if self.table_mgr.df is None:
+                print('df is none')
+                return
+            if self.table_mgr.df.empty:
+                QMessageBox.information(self.main_window, 'Warning', 'Invalid xlsx format, please select correct xlsx')
+                self.config_dialog_mgr.exec()
+                return
+            rvp_cus = {}
+            for row in range(self.ui.tableWidget.rowCount()):
+                for col in range(self.ui.tableWidget.columnCount()):
+                    if row == 0:
+                        continue
+                    value = self.ui.tableWidget.item(row, col).text()
+                    if col == 1 or col == 3:
+                        rvp_cus[self.ui.tableWidget.item(row, col).text()] = self.ui.tableWidget.item(row,
+                                                                                                      col + 1).text()
+                    print('row:' + str(row) + ',  col:' + str(col) + '\n' + value)
 
-        self.ui.pushButton_2.setEnabled(False)
-        self.ui.pushButton_2.setText('Processing...')
-        self.ui.pushButton_2.setStyleSheet("color: red")
-        case_sensitive = self.ui.checkBox.isChecked()
-        print('case_sensitive: ' + str(case_sensitive))
-        self.kwork_thread.send_work_message([2, rvp_cus,case_sensitive])
+            case_sensitive = self.ui.checkBox.isChecked()
+            print('case_sensitive: ' + str(case_sensitive))
+            self.ui.pushButton_2.setEnabled(False)
+            self.ui.pushButton_2.setText('Processing...')
+            self.ui.pushButton_2.setStyleSheet("color: red")
+            self.kwork_thread.send_work_message([2, rvp_cus, case_sensitive, self.config_dialog_mgr.dcfx_path])
 
         pass
 
-class About_Window_Mgr(QDialog):
+
+class Config_Dialog_Mgr(QDialog):
+    xlsx_path = ''
+    dcfx_path = ''
+    xlsx_path_old = ''
+    dcfx_path_old = ''
+    signal_child_2_parent = pyqtSignal(str)
+    def __init__(self, ui, kwork_thread, table_mgr):
+        QDialog.__init__(self)
+        self.kwork_thread = kwork_thread
+        self.ui = ui
+        self.table_mgr = table_mgr
+        self.child=cfg_ui_dialog.Ui_Dialog()#子窗口的实例化
+        self.child.setupUi(self)
+        self.setWindowTitle('Configure')
+        #self.child.label.setText("AutoAllegro (v0.2, Build 1)")
+
+        self.ui.actionConfigure.triggered['bool'].connect(self.exec)
+        self.child.buttonBox.accepted.connect(self.ok_pressed)
+        self.child.buttonBox.rejected.connect(self.cancel_pressed)
+        self.child.pushButton.clicked.connect(self.pushButton_on_click)
+        self.child.pushButton_2.clicked.connect(self.pushButton_2_on_click)
+
+        self.kwork_thread.signal_to_config_dialog.connect(self.emit_handler)
+        self.kwork_thread.send_work_message([4, 'data_4'])
+
+    def event(self, event):
+        #print('SubWindow got event type: '+ str(event.type()))
+        if event.type() == QEvent.Show:
+            print("child dialog is shown!")
+        return super().event(event)
+
+    def emit_handler(self, param_1, param_2):
+        print('emit_handler: \n' + param_1 + '\n' + param_2)
+        self.xlsx_path = self.xlsx_path_old = param_1
+        self.dcfx_path = self.dcfx_path_old = param_2
+        self.child.plainTextEdit.setPlainText(param_1)
+        self.child.plainTextEdit_2.setPlainText(param_2)
+        #self.table_mgr.init_table_default()
+        if os.path.exists(self.xlsx_path):
+            print("auto load xlsx")
+            self.kwork_thread.send_work_message([3, self.xlsx_path])
+        else:
+            print('configured xlsx path does not exist')
+        pass
+
+    def cancel_pressed(self):
+        print('cancel_pressed')
+        if self.xlsx_path_old != self.child.plainTextEdit.toPlainText():
+            self.child.plainTextEdit.setPlainText(self.xlsx_path_old)
+        if self.dcfx_path_old != self.child.plainTextEdit_2.toPlainText():
+            self.child.plainTextEdit_2.setPlainText(self.dcfx_path_old)
+        pass
+    def ok_pressed(self):
+        self.xlsx_path = self.xlsx_path_old = self.child.plainTextEdit.toPlainText()
+        self.dcfx_path = self.dcfx_path_old = self.child.plainTextEdit_2.toPlainText()
+        self.signal_child_2_parent.emit(self.xlsx_path)
+        self.kwork_thread.send_work_message([5, self.xlsx_path, self.dcfx_path])
+        '''
+        if self.xlsx_path:
+            self.signal_child_2_parent.emit(self.xlsx_path)
+        else:
+            print('ok button: xlsx_path is null')
+        if self.dcfx_path:
+            self.kwork_thread.send_work_message([5, self.xlsx_path, self.dcfx_path])
+        else:
+            print('ok button: dcfx_path is null')
+        '''
+    def pushButton_on_click(self):
+        print('pushButton_on_click')
+        #self.dcfx_path = self.child.plainTextEdit_2.toPlainText()
+        self.load_xlsx_file()
+
+    def pushButton_2_on_click(self):
+        print('pushButton_2_on_click')
+        self.load_dcfx_file()
+
+    def load_dcfx_file(self):
+        opened_file_path = ''
+        file_dialog_opened_file_path = QFileDialog.getOpenFileName(None, 'Select File', os.getcwd(), "Excel Files (*.dcfx);;All files (*.*)")
+        print('opened file dialog file: ' + str(file_dialog_opened_file_path[0]))
+        opened_file_path = file_dialog_opened_file_path[0]
+        self.dcfx_path = opened_file_path
+        print('opened file: ' + str(opened_file_path))
+
+        if len(opened_file_path) == 0:
+            print('No file is selected, do nothing \n')
+            return
+        else:
+            self.child.plainTextEdit_2.setPlainText(opened_file_path)
+
+            pass
+            last_profile_name = opened_file_path
+            # LoadProfile(opened_file_path)
+            # load_ini_profile(opened_file_path, 'menu')
+    def load_xlsx_file(self):
+        opened_file_path = ''
+        file_dialog_opened_file_path = QFileDialog.getOpenFileName(None, 'Select File', os.getcwd(), "Excel Files (*.xlsx *.xls);;All files (*.*)")
+        print('opened file dialog file: ' + str(file_dialog_opened_file_path[0]))
+        opened_file_path = file_dialog_opened_file_path[0]
+        self.xlsx_path = opened_file_path
+        print('opened file: ' + str(opened_file_path))
+
+        if len(opened_file_path) == 0:
+            print('No file is selected, do nothing \n')
+            return
+        else:
+            self.child.plainTextEdit.setPlainText(opened_file_path)
+
+            pass
+            last_profile_name = opened_file_path
+            # LoadProfile(opened_file_path)
+            # load_ini_profile(opened_file_path, 'menu')
+
+class about_dialog_mgr(QDialog):
     def __init__(self, ui):
         QDialog.__init__(self)
         self.ui = ui
         self.child=Ui_Dialog()#子窗口的实例化
         self.child.setupUi(self)
         self.setWindowTitle('About AutoAllegro')
-        self.child.label.setText("AutoAllegro (v0.1, Build 1)")
+        self.child.label.setText("AutoAllegro (v0.2, Build 1)")
         self.child.label_2.setText("liukezhao@gmail.com")
         self.child.label_3.setText("<A href='https://autoallegro.html'>https://autoallegro.html</a>")
         self.child.label_3.setOpenExternalLinks(True)
@@ -191,11 +341,12 @@ class Table_Mgr:
         self.ui = ui
         self.main_window = main_window
         self.kwork_thread = kwork_thread
+        self.df = None
         #self.init_table()
         #self.test_excel()
 
         #self.init_table_default()
-        self.kwork_thread.send_work_message([3, 'data_3'])
+        #self.kwork_thread.send_work_message([3, 'data_3'])
 
         self.ui.comboBox_3.addItem('LNL+T4+2230+PCIe4+CNVio3')
 
@@ -226,20 +377,39 @@ class Table_Mgr:
     def init_table_default(self, selected_columns=[]):
         #self.df = pd.read_excel(r'LNL+T4+2230+PCIe4+CNVio3.xlsx', sheet_name='Netname list', header=1)
 
+        # Display an empty table
+        if self.df.empty:
+            #self.ui.tableWidget.clearContents()
+            self.ui.tableWidget.clear()
+            self.ui.tableWidget.setRowCount(0)
+            self.ui.tableWidget.horizontalHeader().setVisible(False)
+            return
+
+        self.ui.tableWidget.clearContents()
+
         self.df = self.df.drop(self.df.columns[0], axis=1)
         nRows = len(self.df.index)
         nColumns = len(selected_columns) or len(self.df.columns)
-        self.ui.tableWidget.setRowCount(nRows)
-        self.ui.tableWidget.setColumnCount(nColumns - 2)
 
-        # Display an empty table
-        if self.df.empty:
-            self.ui.tableWidget.clearContents()
-            return
+        delCols = []
+        for col in range(nColumns):
+            if col == 0:
+                continue
+            item_str = str(self.df.iat[0, col])
+            if item_str != 'Net Names on RVP' and item_str != 'Net Names on customer design':
+                delCols.append(col)
+        for idx, col in enumerate(delCols):
+            self.df = self.df.drop(self.df.columns[col - idx], axis=1)
+
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+        print(self.df)
+
+        self.ui.tableWidget.setRowCount(nRows)
+        # self.ui.tableWidget.setColumnCount(nColumns)
+        self.ui.tableWidget.setColumnCount(5)
 
         columns = selected_columns or self.df.columns.values
-        columns = np.delete(columns,3)
-        columns = np.delete(columns,5)
 
         for idx, col in enumerate(columns):
             print(col)
@@ -254,13 +424,11 @@ class Table_Mgr:
         span_row_cnt = 1
         start_row = 1
 
-        t = self.ui.tableWidget.columnCount()
+        t1 = self.ui.tableWidget.columnCount()
+        t2 = self.ui.tableWidget.rowCount()
         for row in range(self.ui.tableWidget.rowCount()):
             for col in range(self.ui.tableWidget.columnCount()):
-                if col == 3 or col == 4:
-                    item_str = str(self.df.iat[row, col + 1])
-                else:
-                    item_str = str(self.df.iat[row, col])
+                item_str = str(self.df.iat[row, col])
                 if item_str == "nan":
                     item_str = ""
 
--- utility/kworkthread.py
+++ utility/kworkthread.py
@@ -6,6 +6,8 @@ import datetime
 import re
 import pandas as pd
 import numpy as np
+import os.path
+import configparser
 
 from threading import Thread, Event
 from queue import Queue
@@ -16,6 +18,7 @@ logger = klogger.getlog()
 
 class KWorkThread(QThread):
     signal_to_ui = pyqtSignal(str, str, object)
+    signal_to_config_dialog = pyqtSignal(str,str)
     def __init__(self):
         super().__init__()
         self.queue = Queue()
@@ -44,7 +47,7 @@ class KWorkThread(QThread):
             print('=========================')
             print('content: ' + str(msg))
 
-            if len(msg) == 2 or len(msg) == 3:
+            if len(msg) == 2 or len(msg) == 3 or len(msg) == 4:
                 print('msg id: ' + str(msg[0]))
                 print('msg data: ' + str(msg[1]))
                 if msg[0] == 0:
@@ -58,45 +61,106 @@ class KWorkThread(QThread):
                     print('emit signal back msg id: ' + str(msg[0]))
 
                 if msg[0] == 2:
-                    print('working thread handle msg: ' + str(msg[0]))
+                    try:
+                        print('working thread handle msg: ' + str(msg[0]))
 
-                    file_org = r'.\LNL+T4+2230+PCIe4+CNVio3.dcfx'
-                    now = datetime.datetime.now()
-                    current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+                        file_org = msg[3]
+                        now = datetime.datetime.now()
+                        current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-                    file_mod = os.path.splitext(file_org)[0] + '_' + current_time + os.path.splitext(file_org)[1]
+                        file_mod = os.path.splitext(file_org)[0] + '_' + current_time + os.path.splitext(file_org)[1]
 
-                    shutil.copy(file_org, file_mod)
+                        shutil.copy(file_org, file_mod)
 
-                    rvp_cus = msg[1]
+                        rvp_cus = msg[1]
 
-                    delete = [key for key in rvp_cus if key == '' or rvp_cus[key] == '']
+                        delete = [key for key in rvp_cus if key == '' or rvp_cus[key] == '']
 
-                    for key in delete:
-                        del rvp_cus[key]
+                        for key in delete:
+                            del rvp_cus[key]
 
-                    checkWords = rvp_cus.keys()
-                    repWords = rvp_cus.values()
+                        checkWords = rvp_cus.keys()
+                        repWords = rvp_cus.values()
 
-                    kfile = KFile(file_mod)
-                    case_sensitive = msg[2]
-                    print('case_sensitive: ' + str(case_sensitive))
-                    if case_sensitive:
-                        print('case_sensitive, yes')
-                        kfile.replace(checkWords, repWords, True)
-                    else:
-                        print('case_sensitive, no')
-                        kfile.replace(checkWords, repWords)
+                        kfile = KFile(file_mod)
+                        case_sensitive = msg[2]
+                        print('case_sensitive: ' + str(case_sensitive))
+                        if case_sensitive:
+                            print('case_sensitive, yes')
+                            kfile.replace(checkWords, repWords, True)
+                        else:
+                            print('case_sensitive, no')
+                            kfile.replace(checkWords, repWords)
 
-                    self.signal_to_ui.emit(str(msg[0]), 'signal_2', None)
-                    print('emit signal back msg id: ' + str(msg[0]))
+                        self.signal_to_ui.emit(str(msg[0]), 'signal_2', 1)
+                        print('emit signal back msg id: ' + str(msg[0]))
+                    except Exception as e:
+                        print('Exception: ' + str(e))
+                        self.signal_to_ui.emit(str(msg[0]), 'signal_2', 0)
+                        print('emit signal back msg id: ' + str(msg[0]))
 
                 if msg[0] == 3:
                     print('working thread handle msg: ' + str(msg[0]))
-                    df = pd.read_excel(r'LNL+T4+2230+PCIe4+CNVio3.xlsx', sheet_name='Netname list', header=1)
+
+                    if os.path.exists(msg[1]):
+                        try:
+                            df = pd.read_excel(msg[1], sheet_name='Netname list', header=1)
+                        except Exception as e:
+                            print('Exception: ' + str(e))
+                            df = pd.DataFrame()
+                    else:
+                        df = pd.DataFrame()
+                        pass
                     self.signal_to_ui.emit(str(msg[0]), 'signal_3', df)
                     print('emit signal back msg id: ' + str(msg[0]))
+
+                if msg[0] == 4:
+
+                    ini_file = './config.ini'
+                    xlsx_path = ''
+                    dcfx_path = ''
+
+                    if not os.path.exists(ini_file):
+                        self.create_default_ini(ini_file)
+                        print(f"Created {ini_file} with default configuration.")
+                    else:
+                        config = configparser.ConfigParser()
+                        config.read("config.ini")
+
+                        xlsx_path = config.get("file_path", "xlsx")
+                        dcfx_path = config.get("file_path", "dcfx")
+
+                    self.signal_to_config_dialog.emit(xlsx_path, dcfx_path)
+
+                if msg[0] == 5:
+                    ini_file = './config.ini'
+                    xlsx_path = ''
+                    dcfx_path = ''
+                    xlsx_path = msg[1]
+                    dcfx_path = msg[2]
+                    self.write_to_ini(ini_file, xlsx_path, dcfx_path)
+                    pass
 
             self.queue.task_done()
             print('=========================')
 
+    def create_default_ini(self, file_path):
+        config = configparser.ConfigParser()
+        config['file_path'] = {
+            'xlsx': '',
+            'dcfx': ''
+        }
+
+        with open(file_path, 'w') as configfile:
+            config.write(configfile)
+
+    def write_to_ini(self, file_path, xlsx_value, dcfx_value):
+        config = configparser.ConfigParser()
+        config.read(file_path)
+
+        # 写入新的值，如果已存在，则覆盖
+        config.set('file_path', 'xlsx', xlsx_value)
+        config.set('file_path', 'dcfx', dcfx_value)
+
+        with open(file_path, 'w') as configfile:
+            config.write(configfile)
