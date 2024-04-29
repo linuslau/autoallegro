2ac70fde9c6ad6d645f1002f7fcb308b0141f63c
Liu, Kezhao <kezhao.liu@intel.com>
2024-04-29 09:42:14 +0800

    Release version: v0.4.2

--- utility/kappbase.py
+++ utility/kappbase.py
@@ -20,7 +20,7 @@ class KAppBase(object):
         self.app = app
         #self.splash_check()
 
-        self.main_window.setWindowTitle('NetlistAutoMapper v0.4.1')
+        self.main_window.setWindowTitle('NetlistAutoMapper v0.4.2')
 
         self.kwork_thread = KWorkThread()
         self.kwork_thread.start()
@@ -551,7 +551,7 @@ class about_dialog_mgr(QDialog):
         self.child=Ui_Dialog()#子窗口的实例化
         self.child.setupUi(self)
         self.setWindowTitle('About NetlistAutoMapper')
-        self.child.label.setText("NetlistAutoMapper (v0.4.1, Build 1)")
+        self.child.label.setText("NetlistAutoMapper (v0.4.2, Build 1)")
         self.child.label_2.setText("liukezhao@gmail.com")
         self.child.label_3.setText("<A href='https://NetlistAutoMapper.html'>https://NetlistAutoMapper.html</a>")
         self.child.label_3.setOpenExternalLinks(True)
